69ac03e3cf6bb40acaf33108de085973a0dce9c6
PlayitLOUD73 <github@lukewelborn.com>
2022-10-03 16:22:57 -0400

    Initial Commit

---
+++ Just Scores/Just_ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_ScoresApp.swift
+//  Just Scores
+//
+//  Created by Luke Welborn on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Just_ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
